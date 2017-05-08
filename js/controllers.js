c043072e2dba720e36d2b9b5bbe2a63d83239d7a
tetsumote <unionproject88@gmail.com>
2017-05-08 22:06:54 +0200

    com.3 add first controller

---
+++ js/controllers.js
@@ -0,0 +1,13 @@
+var myApp = angular.module('myApp',[]);
+
+myApp.controller('MyController',function MyController($scope){
+	
+
+$scope.author = {
+	name : 'Paul Grajewski',
+	title: 'freelancer',
+	company: 'pawelgrajewski.com.pl'
+};
+	
+	
+});
