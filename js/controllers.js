08a68240d802dfd668b2cef07e3a984938516378
tetsumote <unionproject88@gmail.com>
2017-05-09 01:37:15 +0200

    com.6 styling content

--- js/controllers.js
+++ js/controllers.js
@@ -3,6 +3,7 @@ var myApp = angular.module('myApp',[]);
 myApp.controller('MyController',function MyController($scope){
 	
 
+	
 $scope.artists = [
 	{
 		'realName':'Daisy Ridley',
@@ -58,6 +59,21 @@ $scope.artists = [
 	}
 	
 ];
+
+	
+
+
 	
 	
 });
+
+
+
+
+
+
+
+
+
+
+
