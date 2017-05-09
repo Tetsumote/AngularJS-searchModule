4f2409e2762f4398e05bd1ac1da6cf32e96d624c
tetsumote <unionproject88@gmail.com>
2017-05-09 03:32:17 +0200

    com.10 templating details site

--- js/controllers.js
+++ js/controllers.js
@@ -64,6 +64,66 @@ artistControllers.controller('ListController',function MyController($scope){
 	
 });
 
+artistControllers.controller('DetailsController',function MyController($scope,$routeParams){
+	$scope.artists = [
+	{
+		'realName':'Daisy Ridley',
+		'filmName':'Rey',
+		'discription':'Daisy Jazz Isobel Ridley (born 10 April 1992) is an English actress and film producer. She began her acting career by appearing in minor television roles and short films before being cast in the lead role of Rey in the Star Wars sequel trilogy, appearing in Star Wars: The Force Awakens (2015) and Star Wars: The Last Jedi (2017).',
+	'images':'Daisy_Ridley'
+		
+	},
+	{
+		'realName':'John Boyega',
+		'filmName':'Finn',
+		'discription':'John Adedayo Adegboyega (born 17 March 1992), known professionally as John Boyega, is an English actor and producer best known for playing Finn in the 2015 film Star Wars: The Force Awakens, the seventh film of the Star Wars series. Boyega rose to prominence in his native United Kingdom for his role as Moses in the 2011 sci-fi comedy film Attack the Block.',
+		'images':'John_Boyega'
+		
+		
+	},
+	{
+		'realName':'Mark Hamill',
+		'filmName':'Luke Skywalker',
+		'discription':'Mark Richard Hamill (born September 25, 1951) is an American actor, voice actor, producer and writer. He is known for playing Luke Skywalker in the Star Wars film series. His other works include Corvette Summer (1978) and The Big Red One (1980), among other television shows and movies. Hamill has also appeared on stage in several theater productions, primarily during the 1980s.',
+		'images':'Mark_Hamill'
+		
+	},
+	{
+		'realName':'Oscar Isaac',
+		'filmName':'Poe Dameron',
+		'discription':'Oscar Isaac (born Óscar Isaac Hernández Estrada; March 9, 1979)is a Guatemalan-American actor and musician. He is known for his lead film roles in the comedy-drama Inside Llewyn Davis (2013), for which he received a Golden Globe Award nomination, the crime drama A Most Violent Year (2014) and the science fiction thriller Ex Machina (2015), in which he played Nathan Hamlet Bateman.',
+		'images':'Oscar_Isaac'
+	},
+	{
+		'realName':'Adam Driver',
+		'filmName':'Kylo Ren',
+		'discription':'Adam Douglas Driver (born November 19, 1983) is an American actor. He rose to prominence in the supporting role of Adam Sackler in the HBO comedy-drama series Girls (2012–2017), for which he received three consecutive nominations for the Primetime Emmy Award for Outstanding Supporting Actor in a Comedy Series.',
+		'images':'Adam_Driver'
+	},
+	{
+		'realName':'Benicio Del Toro',
+		'filmName':'Striker',
+		'discription':'Benicio Monserrate Rafael del Toro Sánchez (born February 19, 1967) is a Puerto Rican actor. He won an Academy Award, BAFTA Award, Golden Globe Award, and Screen Actors Guild Award for his portrayal of Javier Rodríguez in the film Traffic (2000).',
+		'images':'Benicio_Del_Toro'
+	},
+	{
+		'realName':'Carrie Fisher',
+		'filmName':'Leia',
+		'discription':'Carrie Frances Fisher (October 21, 1956 – December 27, 2016) was an American actress, writer and humorist who first became known for playing Princess Leia in the Star Wars film series. Her other film roles included Shampoo (1975), The Blues Brothers (1980), Hannah and Her Sisters (1986).',
+		'images':'Carrie_Fisher'
+	},
+	{
+		'realName':'Andy Serkis',
+		'filmName':'Snoke',
+		'discription':'Andrew Clement "Andy" Serkis(born 20 April 1964) is an English film actor, director and author. He is best known for his performance capture roles comprising motion capture acting, animation and voice work for such computer-generated characters as Gollum in The Lord of the Rings film trilogy (2001–2003) and The Hobbit: An Unexpected Journey (2012), King Kong in the eponymous 2005 film, Caesar in the Planet of the Apes reboot series (2011–present).',
+		'images':'Andy_Serkis'
+	}
+	
+];
+	
+	$scope.whichItem = $routeParams.itemId;
+	
+});
 
 
 
